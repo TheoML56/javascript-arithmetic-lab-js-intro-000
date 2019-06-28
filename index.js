0c926d9275f85cb874ca04d0f423544d9206baee
Theo Malaper <theomalaper.cognez@gmail.com>
2019-06-28 23:11:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function add(a, b) {
 
 function substract(a, b) {
    return a - b
-} 
+}
 
 function multiply(a, b) {
   return a * b
@@ -21,7 +21,3 @@ func inc(n) {
 func dec(n) {
   return n--
 }
-
-
-
-
